81230ad8f680c49f22c2524d40efad792c83b782
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-04-06 21:38:52 +0100

    Made changes in the routes file

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Book = require('../model/BookSchema')
 
+
 //@desc    Enter an entry
 //@@route  POST /api/v1/book
 //@@access PUBLIC
@@ -10,10 +11,8 @@ router.post('/', async (req,res) => {
     res.status(201).json({ success:true, data:newBook })
 })
 
-//@desc    GET all entry
-//@@route  GET /api/v1/book
-//@@access PUBLIC
-router.get('/', async (req,res) => {
+
+router.get('/',  async (req,res) => {
     const books = await Book.find()
     if(!books){
         return res.status(404).json({ success:false, data:'No book found' })
@@ -21,16 +20,14 @@ router.get('/', async (req,res) => {
     res.status(200).json({ success:true, data: books, num: books.length})
 })
 
-//@desc    GET an entry
-//@@route  GET /api/v1/book/:id
-//@@access PUBLIC
-router.get('/:id', async (req,res) => {
+router.get('/:id',  async (req,res) => {
   const book = await Book.findById(req.params.id)
   if(!book){
       return res.status(404).json({ success:false, data: 'No book found' })
   }  
   res.status(200).json({ success:true, data: book })
 })
+
 
 //@desc    UPDATE an entry
 //@@route  PUT /api/v1/book/:id
@@ -47,6 +44,7 @@ router.put('/:id', async (req,res) => {
     res.status(200).json({ success:true, data: book })
 })
 
+
 //@desc    DELETE an entry
 //@@route  PUT /api/v1/book/:id
 //@@access PUBLIC
@@ -55,12 +53,10 @@ router.delete('/:id', async (req,res) => {
     res.status(200).json({ success:true, data: 'book deleted' })
 })
 
-//@desc    DELTE all entries
-//@@route  PUT /api/v1/book/
-//@@access PUBLIC
 router.delete('/', async (req,res) => {
     await Book.deleteMany()
    res.status(200).json({ success:true, data: 'All books deleted' })
 })
+
 
 module.exports = router
