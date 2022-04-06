131a7abdc64720792f726a94916192201db6051e
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-04-06 21:51:29 +0100

    Added Comments

--- routes/book.js
+++ routes/book.js
@@ -8,24 +8,33 @@ const Book = require('../model/BookSchema')
 router.post('/', async (req,res) => {
     const { title, author, isbn } = req.body
     const newBook = await Book.create({ title, author, isbn })
-    res.status(201).json({ success:true, data:newBook })
+    res.status(201).json({ 
+        success:true, 
+        data:newBook 
+    })
 })
 
-
+//@desc    GET all entries
+//@@route  GET /api/v1/book
+//@@access PUBLIC
 router.get('/',  async (req,res) => {
     const books = await Book.find()
-    if(!books){
-        return res.status(404).json({ success:false, data:'No book found' })
-    }
-    res.status(200).json({ success:true, data: books, num: books.length})
+    res.status(200).json({ 
+        success:true, 
+        data: books, 
+        num: books.length
+    })
 })
 
+//@desc    GET one entry
+//@@route  GET /api/v1/book/:id
+//@@access PUBLIC
 router.get('/:id',  async (req,res) => {
-  const book = await Book.findById(req.params.id)
-  if(!book){
-      return res.status(404).json({ success:false, data: 'No book found' })
-  }  
-  res.status(200).json({ success:true, data: book })
+  const book = await Book.findById(req.params.id)  
+  res.status(200).json({ 
+      success:true, 
+      data: book 
+    })
 })
 
 
@@ -34,28 +43,37 @@ router.get('/:id',  async (req,res) => {
 //@@access PUBLIC
 router.put('/:id', async (req,res) => {
     let book = await Book.findById(req.params.id)
-    if(!book){
-        return res.status(404).json({ success:false, data: 'No book found' })
-    }  
     book = await Book.findByIdAndUpdate(req.params.id, {$set:req.body}, {
         new:true,
         runValidator:false
     })  
-    res.status(200).json({ success:true, data: book })
+    res.status(200).json({ 
+        success:true, 
+        data: book 
+    })
 })
 
 
 //@desc    DELETE an entry
-//@@route  PUT /api/v1/book/:id
+//@@route  DELETE /api/v1/book/:id
 //@@access PUBLIC
 router.delete('/:id', async (req,res) => {
     await Book.findByIdAndRemove(req.params.id)
-    res.status(200).json({ success:true, data: 'book deleted' })
+    res.status(200).json({ 
+        success:true, 
+        data: 'book deleted' 
+    })
 })
 
+//@desc    DELETE all entries
+//@@route  DELETE /api/v1/book
+//@@access PUBLIC
 router.delete('/', async (req,res) => {
     await Book.deleteMany()
-   res.status(200).json({ success:true, data: 'All books deleted' })
+   res.status(200).json({ 
+       success:true, 
+       data: 'All books deleted' 
+    })
 })
 
 
